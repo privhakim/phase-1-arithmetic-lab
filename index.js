c7b361388b0321fad8ad057552b46181469f636b
privhakim <abdihakim.hassan@student.moringaschool.com>
2023-06-15 09:48:53 +0300

    done work

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
 
+const multiply = num1 * num2;
+console.log(multiply);
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+const num3 = 25;
+const num4 = 7;
+
+const mod = num3 % num4;
+console.log(mod); 
+const numbers = [1, 5, 10, 15, 20];
+const max = Math.max(...numbers);
+console.log(max); 
 // Write your code here
